06421f62a8290ae2a19023b581d92ec065452917
pkanushad <pkanushad57@gmail.com>
2022-11-09 19:43:00 +0530

    allowed host *

--- cargo/cargo/settings.py
+++ cargo/cargo/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j-qoehhf*!e9mt-3_c8sl+wy*zd%8l6*)lp-e#^uq=nx&w==$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1','acerscargo.herokuapp.com']
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['127.0.0.1','acerscargo.herokuapp.com']
 
 
 # Application definition
